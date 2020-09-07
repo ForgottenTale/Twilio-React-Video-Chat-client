8168c3608dee7b08b33ef70130ab12a3f2fe7f6a
Abhijith Kannan <abhijithkannan452@gmail.com>
2020-09-07 11:17:22 +0530

    Update video.js

--- src/components/video.js
+++ src/components/video.js
@@ -1,87 +1,68 @@
-import React,{useEffect,useRef} from 'react';
+import React, { useEffect, useRef } from 'react';
 const TwilioVideo = require('twilio-video');
 
 
 
-function Video({token}){
-    const localVidRef = useRef()
-    const remoteVidRef = useRef()
-    useEffect(() => {
-        TwilioVideo.connect(token, { video: true, audio: true, name: "test" }).then(
-          room => {
-            // Attach the local video
-            TwilioVideo.createLocalVideoTrack().then(track => {
-              localVidRef.current.appendChild(track.attach())
-            })
-    
-            // const addParticipant = participant => {
-            //   console.log("new participant!")
-            //   console.log(participant.identity)
-            //   participant.tracks.forEach(publication => {
-            //     if (publication.isSubscribed) {
-            //       const track = publication.track
-    
-            //       remoteVidRef.current.appendChild(track.attach())
-            //       console.log("attached to remote video")
-            //     }
-            //   })
-    
-            //   participant.on("trackSubscribed", track => {
-            //     console.log("track subscribed")
-            //     remoteVidRef.current.appendChild(track.attach())
-            //   })
-            // }
-    
-            // room.participants.forEach(addParticipant)
-            // room.on("participantConnected", addParticipant)
-            // room.on('participantDisconnected', participant => {
-            //     console.log(`Participant "${participant.identity}" has disconnected from the Room`);
-            //   });
-            room.participants.forEach(participant => {
-                participant.tracks.forEach(publication => {
-                  if (publication.track) {
-                    localVidRef.current.appendChild(publication.track.attach());
-                  }
-                });
-              
-               participant.on('trackSubscribed', track => {
-                localVidRef.current.appendChild(track.attach());
-                });
-              });
-              
+function Video({ token }) {
 
-            room.on('participantConnected', participant => {
-                console.log(`Participant "${participant.identity}" connected`);
-              
-                participant.tracks.forEach(publication => {
-                  if (publication.isSubscribed) {
-                    const track = publication.track;
-                    remoteVidRef.current.appendChild(track.attach());
-                  }
-                });
-              
-                participant.on('trackSubscribed', track => {
-                    remoteVidRef.current.appendChild(track.attach());
-                });
-              });
+  const localVidRef = useRef()
+  const remoteVidRef = useRef()
 
-              room.on('participantDisconnected', participant => {
-                console.log(`Participant disconnected: ${participant.identity}`);
-              });
-              
-              
+  useEffect(() => {
 
-              
-           }
-        )
-      }, [token])
-    return(
+    TwilioVideo.connect(token, { video: true, audio: true, name: "test" }).then(
+      room => {
+        // Attach the local video
+        TwilioVideo.createLocalVideoTrack().then(track => {
+          localVidRef.current.appendChild(track.attach())
+        })
 
-        <div>
-        <div ref={localVidRef} />
-        <div ref={remoteVidRef} />
-      </div>
+        // Attach the participants video
+        room.participants.forEach(participant => {
+          participant.tracks.forEach(publication => {
+            if (publication.track) {
+              localVidRef.current.appendChild(publication.track.attach());
+            }
+          });
+
+          participant.on('trackSubscribed', track => {
+            localVidRef.current.appendChild(track.attach());
+          });
+        });
+
+        // Attach the new participants video
+        room.on('participantConnected', participant => {
+          console.log(`Participant "${participant.identity}" connected`);
+
+          participant.tracks.forEach(publication => {
+            if (publication.isSubscribed) {
+              const track = publication.track;
+              remoteVidRef.current.appendChild(track.attach());
+            }
+          });
+
+          participant.on('trackSubscribed', track => {
+            remoteVidRef.current.appendChild(track.attach());
+          });
+        });
+
+        room.on('participantDisconnected', participant => {
+          console.log(`Participant disconnected: ${participant.identity}`);
+        });
+
+
+
+
+      }
     )
+  }, [token])
+  return (
+
+    <div>
+      <div ref={localVidRef} />
+      <div ref={remoteVidRef} />
+    </div>
+  )
 }
 
 export default Video
