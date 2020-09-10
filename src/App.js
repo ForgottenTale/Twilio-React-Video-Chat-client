1f04cae94448130893900230c1c875feeae64e43
Abhijith Kannan <abhijithkannan452@gmail.com>
2020-09-10 16:10:07 +0530

    login error handle

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Join from './components/join/join';
 import './app.scss';
 import Room from './components/room/room'
 import axios from "axios";
+import imgPic from './components/assets/404.png'
 
 
 function App() {
   const [name, setUsername] = useState("")
   const [roomName, setRoomName] = useState("")
   const [token, setToken] = useState(false);
+  const [handleError, setError ] = useState(false);
 
   const handleSubmit = async event => {
     event.preventDefault()
@@ -18,12 +20,18 @@ function App() {
       roomname:roomName
     }
     var jwt = "";
-    var url = "http://192.168.31.168:5000/jwt/";
+    var url = "";
     await axios.post(url, data).then(res => {
+      if(res.status!==404){
       jwt = res.data.token;
+      }
+      else{
+        setError(true);
+      }
     })
       .catch(error => {
         console.error(error)
+        setError(true);
       })
 
     setToken(jwt)
@@ -36,15 +44,25 @@ function App() {
   const handleRoomNameChange = useCallback(event => {
     setRoomName(event.target.value);
   }, []);
-
-
+  if(handleError){
+    return (
+      <div className="error" >
+       <img src={imgPic} alt="404 Error"/>
+      </div>
+    );
+  }
+else{
   return (
     <div className="App">
+
+     
      {!token ? <Join storeToken={setToken}  handleUsernameChange={handleUsernameChange}
          handleRoomNameChange={handleRoomNameChange}
          handleSubmit={handleSubmit}/> :  <Room roomName={roomName} token={token} setToken={setToken} />}
     </div>
   );
+}
+  
 }
 
 export default App;
