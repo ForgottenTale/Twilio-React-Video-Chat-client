46b405b4495444abcc332838666fac34035da5f5
Abhijith Kannan <abhijithkannan452@gmail.com>
2020-09-11 21:46:42 +0530

    menu update

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { makeStyles } from "@material-ui/core";
 import IconButton from '@material-ui/core/IconButton';
 import MicIcon from '@material-ui/icons/Mic';
 import VideocamIcon from '@material-ui/icons/Videocam';
@@ -7,8 +8,17 @@ import CallEndIcon from '@material-ui/icons/CallEnd';
 
 import './menu.scss'
 
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
+    boxShadow:'0 3px 5px 2px rgba(33, 203, 243, .3)',
+    "&:hover": {
+      backgroundColor: "solid"
+    }
+  }
+});
 function Menu({handleCallDisconnect, handleAudioToggle,handleVideoToggle,handleParticipantListToggle, toggleMenu}) {
-
+  const classes = useStyles();
   let menuClass = 'Menu';
   if(toggleMenu){
     menuClass = 'Menu open';
@@ -16,7 +26,7 @@ function Menu({handleCallDisconnect, handleAudioToggle,handleVideoToggle,handleP
   
   return (
     <div className={menuClass} color="primary">
-      <IconButton color="primary" onClick={handleAudioToggle}>
+      <IconButton color="primary" onClick={handleAudioToggle} className={classes.root}>
         <MicIcon />
       </IconButton>
       <IconButton color="primary" onClick={handleVideoToggle}>
